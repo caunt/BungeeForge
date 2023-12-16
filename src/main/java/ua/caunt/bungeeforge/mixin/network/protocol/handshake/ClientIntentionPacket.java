36288532924686a9e3f82375c1063ea257ddfe1f
Vitalii Kotenko <caunt.official@gmail.com>
2023-12-16 21:28:29 +0200

    forward IP address

--- src/main/java/ua/caunt/bungeeforge/mixin/network/protocol/handshake/ClientIntentionPacket.java
+++ src/main/java/ua/caunt/bungeeforge/mixin/network/protocol/handshake/ClientIntentionPacket.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 
 @Mixin(net.minecraft.network.protocol.handshake.ClientIntentionPacket.class)
 public class ClientIntentionPacket implements ClientIntentionPacketBridge {
+    private String spoofedAddress;
     private UUID spoofedId;
     private Property[] spoofedProperties;
 
@@ -28,8 +29,11 @@ public class ClientIntentionPacket implements ClientIntentionPacketBridge {
         if (chunks.length <= 2)
             return data;
 
+        System.out.println(String.join(", ", chunks));
+
         var properties = gson.fromJson(chunks[3], Property[].class);
 
+        spoofedAddress = chunks[1];
         spoofedId = UUIDTypeAdapter.fromString(chunks[2]);
         spoofedProperties = Arrays.stream(properties)
                 .filter(packet -> !isFmlMarker(packet))
@@ -45,6 +49,11 @@ public class ClientIntentionPacket implements ClientIntentionPacketBridge {
     private static boolean isFmlMarker(Property property)
     {
         return Objects.equals(property.getName(), "extraData") && property.getValue().startsWith("\u0001FML");
+    }
+
+    @Override
+    public String bungee$getSpoofedAddress() {
+        return spoofedAddress;
     }
 
     @Override
--- src/main/java/ua/caunt/bungeeforge/mixin/server/network/ServerGamePacketListenerImpl.java
+++ src/main/java/ua/caunt/bungeeforge/mixin/server/network/ServerGamePacketListenerImpl.java
@@ -1,8 +1,14 @@
 package ua.caunt.bungeeforge.mixin.server.network;
 
+import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ua.caunt.bungeeforge.bridge.network.ConnectionBridge;
+import ua.caunt.bungeeforge.bridge.network.protocol.handshake.ClientIntentionPacketBridge;
 
 @Mixin(net.minecraft.server.network.ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImpl {
